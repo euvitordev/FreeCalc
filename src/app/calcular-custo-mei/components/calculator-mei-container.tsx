67f9c1d9dac57761cf094fa45b8ba4ad868ffb4d
euvitordev <dev.vitorlucas@gmail.com>
2023-12-29 01:43:24 -0300

    update: Interface improvements and button to report bugs added

--- src/app/calcular-custo-mei/components/calculator-mei-container.tsx
+++ src/app/calcular-custo-mei/components/calculator-mei-container.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   BadgePercent,
+  Bug,
   Calculator,
   ChevronLeft,
   Copy,
@@ -78,31 +79,53 @@ export default function CalculatorMeiContainer() {
     <>
       <div className="flex h-full w-full gap-4 rounded-xl bg-white max-xl:flex-col max-lg:mx-auto max-lg:gap-0">
         <div className="h-full w-full items-center justify-center max-lg:h-screen max-lg:rounded-none">
-          <div className="flex h-full max-w-4xl flex-col items-start justify-start gap-8 p-8">
-            <div className="flex w-full items-center gap-4">
-              <Link href={"/"}>
+          <div className="flex h-full max-w-4xl flex-col items-start justify-start gap-8 p-8 max-xl:p-6">
+            <div className="flex w-full items-center justify-between">
+              <Link
+                href={"/"}
+                className="flex items-center gap-2 rounded-full bg-[rgb(220,224,217)] p-1 pr-6 font-bold text-[rgb(22,51,0)] hover:bg-zinc-200"
+              >
                 <Button
-                  className="rounded-lg bg-zinc-50/25 text-[rgb(22,51,0)] hover:text-black"
+                  className="h-8 w-8 rounded-full bg-[rgb(159,232,112)] text-[rgb(22,51,0)] hover:text-black"
                   variant={"secondary"}
                   size={"icon"}
                 >
-                  <MoveLeft />
+                  <MoveLeft size={18} />
+                </Button>
+                Voltar
+              </Link>
+              {/*  */}
+              <Link
+                href={"https://github.com/euvitordev/FreeCalc/issues"}
+                target="_blank"
+                className="flex items-center gap-2 rounded-full bg-[rgb(220,224,217)] p-1 pl-6 font-bold text-[rgb(22,51,0)] hover:bg-zinc-200"
+              >
+                Encontrou algum bug?
+                <Button
+                  className="h-8 w-8 rounded-full bg-[rgb(159,232,112)] text-[rgb(22,51,0)] hover:text-black"
+                  variant={"secondary"}
+                  size={"icon"}
+                >
+                  <Bug size={18} />
                 </Button>
               </Link>
+            </div>
+
+            <div className="flex w-full items-center">
               <h1 className="text-2xl font-bold text-[rgb(22,51,0)] max-lg:text-xl">
                 Vamos calcular seus custos como MEI!
               </h1>
             </div>
 
-            <div className="overflow-container flex h-screen w-full flex-col items-center gap-4 overflow-x-auto">
+            <div className="overflow-container flex h-screen w-full flex-col items-start gap-2 overflow-x-auto">
               <form
-                className="flex h-full w-full flex-col gap-4 overflow-y-auto max-lg:text-start"
+                className="flex h-full w-full flex-col gap-2 overflow-y-auto max-lg:text-start"
                 onSubmit={handleSubmit}
               >
                 {inputFields.map((field, index) => (
                   <div
                     key={field.id}
-                    className="flex w-full flex-col items-start p-2 text-[rgb(22,51,0)]"
+                    className="flex w-full flex-col items-start p-1 text-[rgb(22,51,0)]"
                   >
                     <div className="flex h-full w-full flex-col justify-between gap-2">
                       <Label
@@ -122,7 +145,7 @@ export default function CalculatorMeiContainer() {
                     </div>
                   </div>
                 ))}
-                <div className="flex w-full p-2">
+                <div className="mt-4 flex w-full p-2">
                   <Button
                     type="submit"
                     className="w-full bg-[rgb(159,232,112)] p-8 text-xl font-bold text-[rgb(22,51,0)] hover:bg-[rgb(139,205,99)]"
@@ -135,7 +158,7 @@ export default function CalculatorMeiContainer() {
           </div>
         </div>
 
-        <div className="flex h-full w-full flex-col items-start gap-4 border-l-2 p-8 max-xl:flex max-lg:rounded-none max-lg:border-t-2">
+        <div className="flex h-full w-full flex-col items-start gap-8 border-l-2 p-8 max-xl:flex max-xl:p-6 max-lg:rounded-none max-lg:border-t-2">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
               <FileLineChart />
@@ -155,7 +178,7 @@ export default function CalculatorMeiContainer() {
             </div>
             {/*  */}
             <div className="flex w-full flex-col justify-between gap-4 max-lg:text-start">
-              <div className="flex h-full w-full gap-8 rounded-xl p-4">
+              <div className="flex h-full w-full gap-8 rounded-xl bg-[rgb(237,239,235)] p-4">
                 <div className="flex items-center">
                   <span className="rounded-full bg-[rgb(220,224,217)] p-2 text-[rgb(22,51,0)]">
                     <DollarSign />
@@ -173,7 +196,7 @@ export default function CalculatorMeiContainer() {
                   </p>
                 </div>
               </div>
-              <div className="flex w-full gap-8 rounded-xl p-4 ">
+              <div className="flex w-full gap-8 rounded-xl bg-[rgb(237,239,235)] p-4">
                 <div className="flex items-center">
                   <span className="rounded-full bg-[rgb(220,224,217)] p-2 text-[rgb(22,51,0)]">
                     <DollarSign />
